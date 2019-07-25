81d835c0ee1a80ddd4afc6ccd84385ac72270d9f
william.cho <william.cho@kakaomobility.com>
2019-07-25 22:23:11 +0900

    admin에 Choice 모델 추가, detail.html 오타 수정

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
-
-admin.site.register(Question)
+myModels = [Question, Choice]
+admin.site.register(myModels)
